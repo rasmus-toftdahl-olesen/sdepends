68868d831a80fbfb9aed9be0d8222e31bada7d7f
rasmus.toftdahl.olesen <rasmus.toftdahl.olesen@3b5d4ceb-8f5f-a3f8-e408-35ed142aaab6>
2012-03-23 07:43:41 +0000

    Import initial source code.

---
+++ trunk/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SDepends
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] _args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MainForm mainForm = new MainForm();
+            if (_args.Length > 0)
+            {
+                mainForm.Open(_args[_args.Length - 1]);
+            }
+            Application.Run(mainForm);
+        }
+    }
+}
